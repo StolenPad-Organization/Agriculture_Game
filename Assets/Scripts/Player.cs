2039c746df80a1ecd75a4515b84a8a4ac0a633b1
youssefboussaid <85492375+youssefboussaid@users.noreply.github.com>
2024-01-30 17:21:35 +0100

    feat : player interaction

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,33 +7,73 @@ public class Player : MonoBehaviour
     [SerializeField] private GameInput gameInput;
     [SerializeField] private PlayerAnimator playerAnimator;
     [SerializeField] private float moveSpeed = 4f;
-    [SerializeField] private float rotateSpeed = 10f;
+    [SerializeField] float rotateSpeed = 10f;
+    [SerializeField] Transform playerHoldPoint;
 
 
+    private enum State
+    {
+        Idle,
+        Walking,
+        Pushing,
+        Driving,
+    }
+
+
+    private State state;
 
     private bool isWalking;
+    private bool isPushing;
+
 
     void Start()
     {
-
+        state = State.Idle;
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        HandleMovement();
+        switch (state)
+        {
+            case State.Idle:
+                HandleIdle();
+                break;
+            case State.Walking:
+                HandleMovement();
+                break;
+            case State.Pushing:
+                HandlePushing();
+                break;
+        }
+
     }
 
+
+
+    private void HandleIdle()
+    {
+        Vector2 inputVector = gameInput.GetMovementVector();
+        if (inputVector != Vector2.zero)
+        {
+            state = State.Walking;
+            isWalking = true;
+            playerAnimator.SetWalkingState(true);
+        }
+    }
     private void HandleMovement()
     {
-
         Vector2 inputVector = gameInput.GetMovementVector();
         if (inputVector == Vector2.zero)
         {
+            //player is not moving 
             if (isWalking)
             {
-                playerAnimator.SetAnimatorSate(false);
+                //player was moving and then stop
+                playerAnimator.SetWalkingState(false);
                 isWalking = false;
+                state = State.Idle;
             }
             return;
         }
@@ -42,11 +82,64 @@ public class Player : MonoBehaviour
         transform.forward = Vector3.Slerp(transform.forward, moveDir, rotateSpeed * Time.deltaTime);
         if (!isWalking)
         {
-            playerAnimator.SetAnimatorSate(true);
+
+            //player was not moving and then started to move
+            playerAnimator.SetWalkingState(true);
             isWalking = true;
+            state = State.Walking;
         }
 
     }
+
+
+    private void HandlePushing()
+    {
+        Vector2 inputVector = gameInput.GetMovementVector();
+        if (inputVector == Vector2.zero)
+        {
+            
+            if (isPushing)
+            {
+                
+                playerAnimator.SetPushingState(true);
+                playerAnimator.SetPushSpeed(0);
+                isPushing = false;
+            }
+            return;
+        }
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, rotateSpeed * Time.deltaTime);
+        if (!isPushing)
+        {
+            playerAnimator.SetWalkingState(true);
+            playerAnimator.SetPushSpeed(1);
+            isPushing = true;
+        }
+    }
+
+
+
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    if (collision.transform.TryGetComponent(out FarmingTool farmingTool))
+    //    {
+    //        farmingTool.PlayerUseTool(playerHoldPoint);
+    //        playerAnimator.SetPushingState(true);
+    //        state = State.Pushing;
+    //    }
+    //}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.TryGetComponent(out FarmingTool farmingTool))
+        {
+            farmingTool.PlayerUseTool(playerHoldPoint);
+            playerAnimator.SetPushingState(true);
+            state = State.Pushing;
+        }
+    }
+
 
 
     public bool IsWalking()
@@ -54,5 +147,5 @@ public class Player : MonoBehaviour
         return isWalking;
     }
 
-
+  
 }
--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -6,15 +6,31 @@ public class PlayerAnimator : MonoBehaviour
 {
 
     private const string IS_WALKING = "IsWalking";
+    private const string IS_PUSHING = "IsPushing";
+    private const string PUSH_SPEED = "PushSpeed";
+
     private Animator animator;
     void Start()
     {
         animator = GetComponent<Animator>();
     }
     
-    public void SetAnimatorSate(bool isWalking)
+
+
+    public void SetWalkingState(bool isWalking)
     {
-        Debug.Log("SetAnimatorSate");
         animator.SetBool(IS_WALKING, isWalking);
     }
+
+    public void SetPushingState(bool isPushing)
+    {
+        animator.SetBool(IS_PUSHING, isPushing);
+    }
+
+    public void SetPushSpeed(int speedAmount)
+    {
+        animator.SetFloat(PUSH_SPEED, speedAmount);
+    }
+
+
 }
